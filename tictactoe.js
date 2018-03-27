a7037326fb0d68f50a4dce58012116294a985c84
artie c <icewizardwoe@mail.com>
2018-03-26 20:55:40 -0400

    finished computer moves and player moves with random chosens, may use minimax in future

--- tictactoe.js
+++ tictactoe.js
@@ -6,11 +6,11 @@ function game () {
   this.playerMoves = [];
   this.computerMoves = [];
   this.squares = document.querySelectorAll('.play');
-  this.spotsLeft = [1,2,3,4,5,6,7,8,9];
+  this.spotsLeft = [0,1,2,3,4,5,6,7,8];
   this.winningCombos = [
-    [1, 2, 3],[4, 5, 6],[7, 8, 9],
-    [1, 4, 7],[2, 5, 8],[3, 6, 9],
-    [1, 5, 9],[3, 5, 7]
+    [0, 1, 2],[3, 6, 5],[6, 7, 8],
+    [0, 3, 6],[1, 4, 7],[2, 5, 8],
+    [0, 4, 8],[2, 4, 6]
   ];
 
   for(let i = 0; i < this.squares.length; i++){
@@ -31,19 +31,45 @@ game.prototype.chooseSymbol = function (symbol) {
 
 game.prototype.playerMove = function (i) {
     if(this.squares[i].classList.contains('play')){
+        
         this.squares[i].textContent = this.player;
-        console.log(this.squares[i].id);
-        //remove to prevent overwriting text content on computer move
+        
+        //remove to prevent overwriting text content on computer move and make player move array
         this.squares[i].classList.remove('play');
-        console.log(this.squares);
-        console.log(typeof(Number(this.squares[i].id)));
-        this.playerMoves.push(Number(this.squares[i].id));
-        console.log(this.playerMoves);
+        
+        //remove spot player chose to prevent computer from moving here
+        let index = this.spotsLeft.indexOf(i);
+        
+        this.spotsLeft.splice(index, 1);
+        
+        //push to array to compare for win
+        this.playerMoves.push(i); 
+        
+        setInterval(this.computerMove(), 2000);
+
+        console.log(this.playerMoves + ' player');
     }
 }
 
+//chooses spot randomly
 game.prototype.computerMove = function () {
-   
+    let random = this.spotsLeft[Math.floor(Math.random() * this.spotsLeft.length)]
+    if(!this.squares[random]){
+        console.log('tie');
+        return;
+    }
+    this.squares[random].textContent = this.computer;
+    
+    //remove to prevent choosing again
+    let index = this.spotsLeft.indexOf(random);
+    
+    this.spotsLeft.splice(index, 1);
+    
+    this.squares[random].classList.remove('play');
+
+    this.computerMoves.push(random);
+
+    console.log(this.computerMoves + ' computer');
 }
 
 const ticTacToe = new game();
